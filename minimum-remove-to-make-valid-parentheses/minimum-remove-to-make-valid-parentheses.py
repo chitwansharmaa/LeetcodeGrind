557bbd1dc046529c7868d74c471defb7594394cb
Chitwan Sharma <64715607+chitwansharmaa@users.noreply.github.com>
2021-11-22 14:55:52 -0500

    Time: 102 ms (78.07%), Space: 15.9 MB (60.28%) - LeetHub

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        #creating a stack to keep track of index number for open parenthesis
+        stack = []
+        N = len(s)
+        #converting string to list so that popping is easier
+        S = list(s)
+        for i in range(N):
+            if S[i] == "(":
+                #if open parenthesis we storing the value of i
+                stack.append(i)
+            elif S[i] == ")":
+                #if we have stack we open parenthesis value we will pop the index as it is valid parenthesis
+                if stack:
+                    stack.pop()
+                else:
+                    S[i] = ""
+            
+        for j in stack:
+            S[j] = ""
+        return "".join(S)
